432f319a06c70b48af816d24134da6bfbf1e018a
Ryan Alexander Siu <31353270+ryansiu17@users.noreply.github.com>
2019-05-08 23:28:13 -0700

    getMatchInfo complete

---
+++ getMatchInfo.js
@@ -0,0 +1,56 @@
+const puppeteer = require("puppeteer");
+const { convertArrayToCSV } = require("convert-array-to-csv");
+const fs = require("fs");
+
+const urls = [
+  "https://www.hltv.org/stats/matches/mapstatsid/79888/flash-vs-mvp-pk",
+  "https://www.hltv.org/stats/matches/mapstatsid/85427/liquid-vs-fnatic?contextIds=4991&contextTypes=team",
+  "https://www.hltv.org/stats/matches/mapstatsid/74787/faze-vs-g2"
+];
+
+let scrape = async url => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const result = await page.evaluate(() => {
+    let data = {}; // Each match is an object, must be put in array before converted to csv
+    data.matchId = window.location.href.split("/")[6];
+    data.tournament = document.querySelector(".match-info-box a").innerText;
+    data.date = document.querySelector(
+      ".match-info-box .small-text span"
+    ).innerText;
+    data.map = document
+      .querySelector(".match-info-box")
+      .childNodes[3].nodeValue.trim();
+    data.team1 = document.querySelector(".team-left img").title;
+    data.team2 = document.querySelector(".team-right img").title;
+    data.winner = document
+      .querySelector(".bold.won")
+      .parentElement.querySelector("img").title;
+    data.team1_rounds = document.querySelector(".team-left .bold").innerText;
+    data.team2_rounds = document.querySelector(".team-right .bold").innerText;
+    return data;
+  });
+  browser.close();
+  return result;
+};
+
+async function processUrls() {
+  const out = [];
+  for (const url of urls) {
+    const output = await scrape(url);
+    out.push(output);
+  }
+  fs.writeFile("output.csv", convertArrayToCSV(out), error => {
+    console.log(error);
+  });
+}
+
+processUrls();
+
+/* const output = convertArrayToCSV(out);
+fs.writeFile("output.csv", output, "utf8", error => {
+  console.log(error);
+});
+ */
--- test.js
+++ test.js
@@ -2,29 +2,19 @@ const puppeteer = require("puppeteer");
 const { convertArrayToCSV } = require("convert-array-to-csv");
 const fs = require("fs");
 
+const url =
+  "https://www.hltv.org/stats/matches/mapstatsid/85427/liquid-vs-fnatic?contextIds=5973&contextTypes=team";
+
 let scrape = async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto("https://www.hltv.org/ranking/teams/2019/may/6");
+  await page.goto(url);
 
   const result = await page.evaluate(() => {
     let data = []; // Create an empty array that will store our data
-    let teams = document.querySelectorAll(".ranked-team"); // Select all Products
-
-    for (var element of teams) {
-      let team = {};
-      team.position = element.querySelector(".position").innerText;
-      team.name = element.querySelector(".name").innerText;
-      let players = element.querySelectorAll(".rankingNicknames");
-      for (let i = 0; i < players.length; i++) {
-        team["player" + i] = players[i].innerText;
-      }
-      data.push(team);
-    }
     return data; // Return our data array
   });
-
   browser.close();
   return result; // Return the data
 };
@@ -32,5 +22,7 @@ let scrape = async () => {
 scrape().then(value => {
   console.log(value); // Success!
   const output = convertArrayToCSV(value);
-  fs.writeFile("output.csv", output);
+  fs.writeFile("output.csv", output, "utf8", error => {
+    console.log(error);
+  });
 });
